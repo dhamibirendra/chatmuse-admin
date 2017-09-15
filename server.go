4f7b014a185eb86674bee9972d26d0a5188ab946
birendradhami <dhamibirendra@gmail.com>
2017-09-15 12:32:48 +0545

    add fixtures at startup

--- server.go
+++ server.go
@@ -18,6 +18,8 @@ func main() {
 
 	e := echo.New()
 
+	runFixture()
+
 	// the file server for rice. "app" is the folder where the files come from.
 	publicAssetHandler := http.FileServer(rice.MustFindBox("public").HTTPBox())
 
@@ -218,4 +220,41 @@ func sendEmail(subject string, message string) {
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
+}
+
+func runFixture() {
+	p1 := &product{
+		ID:          1,
+		Title:       "Wildcraft Laptop Bags",
+		Description: "Wildcraft Laptop Bags with Description",
+		Price:       1419,
+		WebUrl:      "https://smartdoko.com/shop/wildcraft-gravity-nylon-bag-8903338009566",
+		ImageUrl:    "https://smartdoko.com/assets/upload/images/product/detail/IMG-0600cc412ddc0ff29dd2e437b89f9cc1.gif",
+		Payload:     "flsymAvu",
+	}
+
+	p2 := &product{
+		ID:          2,
+		Title:       "Samsung Gear S2 Platinum",
+		Description: "Step up your style with the Samsung Gear S2 classic. Genuine leather, precious metal and exceptional finishes come together to create a sophisticated wearable that goes with anything.",
+		Price:       2040,
+		WebUrl:      "https://smartdoko.com/shop/samsung-galaxy-s2-sports-gear-band",
+		ImageUrl:    "https://smartdoko.com/assets/upload/images/product/detail/IMG-23d641578ee1dab84459058b8a02e1ee.jpg",
+		Payload:     "krvixZGh",
+	}
+
+	p3 := &product{
+		ID:          3,
+		Title:       "Multipurpose Water Spray Gun ",
+		Description: "Expandable & Flexible Plastic Water Garden Pipe with Spray Nozzle For Car Wash Pet Bath",
+		Price:       1395,
+		WebUrl:      "https://smartdoko.com/shop/multipurpose-water-spray-gun",
+		ImageUrl:    "https://smartdoko.com/assets/upload/images/product/detail/IMG-332b397997496f4c2bcdf1d24ebb814a.gif",
+		Payload:     "cQxAVrlD",
+	}
+
+	products[p1.ID] = p1
+	products[p2.ID] = p2
+	products[p3.ID] = p3
+
 }
